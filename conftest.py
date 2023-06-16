80ccb8cba758b4075dd8d80126afbfe2c05f917b
FArooghOghba <faroogh.oghba@gmail.com>
2023-06-17 00:52:00 +0330

    built: create test for custom user model

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 
+from rest_framework.test import APIClient
+
 import pytest
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 
 @pytest.fixture
--- movie_recommendation_api/common/models.py
+++ movie_recommendation_api/common/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.db.models.query import F, Q
-from django.utils import timezone
 
 
 class BaseModel(models.Model):
     """
     This is a base model, The fields of this model are probably used in all models.
     """
-    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -29,3 +28,4 @@ class RandomModel(BaseModel):
                 check=Q(start_date__lt=F("end_date"))
             )
         ]
+        abstract = True
