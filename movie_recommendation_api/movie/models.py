29bb5e952797615931fe65e43a0dbad2bc1e0416
FArooghOghba <faroogh.oghba@gmail.com>
2023-06-24 12:05:57 +0330

    build: genre model for movie app

--- movie_recommendation_api/movie/models.py
+++ movie_recommendation_api/movie/models.py
@@ -1,3 +1,16 @@
-from django.db import models # noqa
+from django.db import models
+
+from movie_recommendation_api.common.models import BaseModel
+
 
 # Create your models here.
+
+class Genre(BaseModel):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
