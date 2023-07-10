0e4f7f95154ee4edc272406b201ff11540b21221
FArooghOghba <faroogh.oghba@gmail.com>
2023-07-10 11:28:16 +0330

    docs: add docs for rating fixtures

--- movie_recommendation_api/tests/fixtures/rating_fixtures.py
+++ movie_recommendation_api/tests/fixtures/rating_fixtures.py
@@ -5,11 +5,25 @@ from movie_recommendation_api.tests.factories.movie_factories import RatingFacto
 
 @pytest.fixture
 def first_test_rating():
+    """
+    Fixture for creating a test rating.
 
+    This fixture creates and returns a test rating object using
+    the `RatingFactory` factory.
+
+    :return: Rating: A test rating object.
+    """
     return RatingFactory()
 
 
 @pytest.fixture
 def second_test_rating():
+    """
+    Fixture for creating a test rating.
 
+    This fixture creates and returns a test rating object using
+    the `RatingFactory` factory.
+
+    :return: Rating: A test rating object.
+    """
     return RatingFactory()
