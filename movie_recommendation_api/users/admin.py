0b062a956c67cff0c82db579d4a17dacd58fa711
FArooghOghba <faroogh.oghba@gmail.com>
2023-07-27 22:36:05 +0330

    feat: register admin for user models

--- movie_recommendation_api/users/admin.py
+++ movie_recommendation_api/users/admin.py
@@ -1,3 +1,40 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from movie_recommendation_api.users import models
+
+
+@admin.register(models.BaseUser)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        'email', 'username', 'is_active',
+        'is_staff', 'is_verified', 'is_superuser'
+    )
+    list_filter = ('is_active', 'is_staff', 'is_verified', 'is_superuser')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+    fieldsets = (
+        ('Authentication', {
+            'fields': ('email', 'username', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_active', 'is_verified', 'is_superuser')
+        }),
+        ('Group Permissions', {
+            'fields': ('groups', 'user_permissions')
+        }),
+        ('Important Date', {
+            'fields': ('last_login',)
+        }),
+    )
+
+    add_fieldsets = (
+        ('Authentication', {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2'),
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_active', 'is_verified', 'is_superuser')
+        }),
+    )
