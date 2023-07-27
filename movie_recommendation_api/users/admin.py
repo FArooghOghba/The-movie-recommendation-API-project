e964b73c99b4744f4ec174b750ae2e9fc6227ec5
FArooghOghba <faroogh.oghba@gmail.com>
2023-07-27 22:42:58 +0330

    docs: add docs and comment for user admin

--- movie_recommendation_api/users/admin.py
+++ movie_recommendation_api/users/admin.py
@@ -6,6 +6,17 @@ from movie_recommendation_api.users import models
 
 @admin.register(models.BaseUser)
 class UserAdmin(BaseUserAdmin):
+    """
+    Custom UserAdmin class for the BaseUser model.
+
+    This class customizes the Django admin interface for the BaseUser model.
+    It defines the fields to display in the list view, the filters to apply,
+    the fields to search, and the ordering of the users.
+
+    It also customizes the fieldsets to display when adding or editing a user,
+    and defines custom fieldsets for adding a new user.
+    """
+
     list_display = (
         'email', 'username', 'is_active',
         'is_staff', 'is_verified', 'is_superuser'
@@ -14,6 +25,7 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('email', 'username')
     ordering = ('email',)
 
+    # Fieldsets to display when adding or editing a user
     fieldsets = (
         ('Authentication', {
             'fields': ('email', 'username', 'password')
@@ -29,6 +41,7 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
+    # Custom fieldsets for adding a new user
     add_fieldsets = (
         ('Authentication', {
             'classes': ('wide',),
