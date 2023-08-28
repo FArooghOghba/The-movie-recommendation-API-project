23d533147676473743d995da20e619fc623384a5
FArooghOghba <faroogh.oghba@gmail.com>
2023-08-28 14:37:48 +0330

    fix: get setting from .env
    getting DJANGO_SETTINGS_MODULE from .env file

--- config/celery.py
+++ config/celery.py
@@ -1,7 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.django.local')
+from config.env import env
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('DJANGO_SETTINGS_MODULE'))
 
 celery = Celery('config')
 celery.config_from_object('django.conf:django', namespace='CELERY')
