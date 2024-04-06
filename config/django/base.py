896244519ffce265ea615bd29b8a8a2d252801e4
FArooghOghba <faroogh.oghba@gmail.com>
2024-04-06 13:06:27 +0330

    fix: edit static root address

--- config/django/base.py
+++ config/django/base.py
@@ -139,8 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/vol/web/static')
+MEDIA_ROOT = '/vol/web/media'
+
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
